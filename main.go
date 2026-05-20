bfc9c8612f3cf9cd0d3676ea969627b37982343b
DexterDebugs <munig.shankar@gmail.com>
2026-05-20 15:01:49 +0530

    feat: RESP parser - reads array of bulk strings into []string

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
+	"strconv"
+	"io"
 	"strings"
 )
+
 //-------------------------------------SERVER--------------------------------------------------
 func handleConnection(conn net.Conn){
 	defer conn.Close()	//works after surrounding functions are executed
@@ -29,7 +33,65 @@ func handleConnection(conn net.Conn){
 	}
 }
 
+//----------------------------------------PARSER FUNCTION---------------------------------------------------------
+func readCommand(reader *bufio.Reader) ([] string, error){	//takes raw protocol bytes off a wire and turns them into a clean, usable data structure
+	//read the array header	- "*3\r\n"
+	line, err := reader.ReadString('\n')	//returns string and error
+	if (err != nil){
+		return nil, err
+	}
+
+	//Strip the '*' and the trailing \r\n
+	line = strings.TrimSpace(line)		//you are FORGETTING to return function values by re-assigning them
+
+	if len(line) == 0 || line[0] != '*'{		//if first char is not *, return error
+		return nil,fmt.Errorf("expected '*', got %q", line)
+	}
+	n, err := strconv.Atoi(line[1:])		//line[1:] returns a string
+	if (err != nil){				//atoi takes string as input, returns int, err as output
+		return nil, err
+	}
+	results := make([]string, 0, n)		//make string array with 0 length, n capacity
+	for i := 0; i < n; i++ {	//read 'n' bulk strings
+		header, err := reader.ReadString('\n')	//read "$3\r\n"
+		if err != nil{
+			return nil, err
+		}
+		header = strings.TrimSpace(header)	//"$3"
+
+		if len(header) == 0 || header[0] != '$'{	//check if starts with '$'
+			return nil, fmt.Errorf("expected '$', got '%q'", header)
+		}
+		length, err:= strconv.Atoi(header[1:])
+		if err != nil {
+			return nil, err
+		}
+
+		buf := make([]byte, length)
+		_, err = io.ReadFull(reader, buf)
+		if err != nil {
+			return nil, err
+		}
+		reader.Discard(2)
+		results = append(results, string(buf))
+	}
+	return results, nil
+}
+//----------------------------TESTING PARSER-----------------------------------------------
+/*func testParser() {
+	raw := "*3\r\n$3\r\nSET\r\n$4\r\nname\r\n$4\r\nAlex\r\n"
+	reader := bufio.NewReader(strings.NewReader(raw))
+	cmd, err := readCommand(reader)
+	if err != nil {
+		fmt.Println("error: ", err)
+		return
+	}
+	fmt.Println("parsed: ", cmd)
+}*/
+
 func main(){
+	//testParser()	used for testing purposes
+
 	ln,err := net.Listen("tcp", ":6379")	//ln is a listener object
 	if err != nil {		//if error occurs, print it to the console
 		fmt.Println("error: ", err)
